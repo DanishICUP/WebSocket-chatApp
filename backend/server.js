fdbcf24a4712e93ebad8e381cd9d430ca19a5b98
DanishICUP <danishicp99@gmail.com>
2026-02-04 14:56:52 +0500

    all commit

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+import express from 'express'
+import { Socket } from 'node:dgram'
+
+const app = express()
+const PORT = 5000
+
+const server = createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+})
+
+const ROOM = "room"
+
+io.on('connection', (socket) => {
+    console.log("User connected to server:", socket.id)
+
+    socket.on("joinRoom", async (userName) => {
+
+        console.log(`${userName} joined the room`)
+        await socket.join(ROOM)
+
+        //send to all
+        // io.to(ROOM).emit("RoomNotice", userName)
+
+        //send to all except sender
+        socket.to(ROOM).emit("RoomNotice", userName)
+
+        //send message event
+        socket.on("chatMessages", (msg) => {
+            socket.to(ROOM).emit("chatMessages", msg)
+        })
+
+        //typers 
+        socket.on('typing', (userName) => {
+            socket.to(ROOM).emit('typing', userName)
+        })
+
+        //stop typing litening event 
+        socket.on("stopTyping", (userName) => {
+            socket.to(ROOM).emit("stopTyping", userName)
+        })
+    })
+
+})
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+server.listen(PORT, () => {
+    console.log(`server is running on port:${PORT}`)
+})
